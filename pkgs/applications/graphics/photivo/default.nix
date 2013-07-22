e9130d4a4887752d065a307ea7eba19de19feb6d
Petr Rockai <me@mornfall.net>
2013-07-22 14:06:32 +0200

    photivo: Fix paths.

--- pkgs/applications/graphics/photivo/default.nix
+++ pkgs/applications/graphics/photivo/default.nix
@@ -6,11 +6,16 @@ stdenv.mkDerivation rec {
 
   src = fetchhg {
     url = "http://code.google.com/p/photivo/";
-    tag = "6256ff175312";
-    sha256 = "0pyvkijr7wwik21hdp1zwbbyqnhc07kf0m48ih1rws78fq3h86cc";
+    tag = "b6e851283ac3";
+    sha256 = "1wsqisbv6j80aylk4jr83smyzc2n85h98xiiabgpbdxw7q9n15jv";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
 
   buildInputs = [ qt4 fftw graphicsmagick_q16 lcms2 lensfun libjpeg exiv2 liblqr1 ];
+  patchPhase = '' # kinda icky
+    sed -e '/("@INSTALL@")/d' \
+        -e s,@INSTALL@,$out/share/photivo, \
+        -i Sources/ptSettings.cpp
+  '';
 }
