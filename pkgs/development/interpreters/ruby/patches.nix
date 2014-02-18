d4661845513f1c95b9af971a19126eb2403230b4
Petr Rockai <me@mornfall.net>
2014-02-18 14:55:43 +0100

    ruby-bundler: Do not fail in patchShebangs.

--- pkgs/development/interpreters/ruby/patches.nix
+++ pkgs/development/interpreters/ruby/patches.nix
@@ -27,6 +27,13 @@ in
     '';
   };
 
+  bundler = {
+    dontPatchShebangs = 1;
+    postInstall = ''
+        patchShebangs $out/bin/bundle{,_ruby}
+    '';
+  };
+
   ffi = {
     postUnpack = "onetuh";
     buildFlags = ["--with-ffi-dir=${libffi}"];
@@ -132,6 +139,4 @@ in
         zcat ${patch} | patch -p 1
       ''; # */
     };
-
-  bundler = { dontPatchShebangs=1; };
 }
