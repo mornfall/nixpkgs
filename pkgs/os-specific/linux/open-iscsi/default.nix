cc9da6531572adef231ae7cfbd1dcc34ab2c2087
Petr Rockai <me@mornfall.net>
2014-11-14 22:48:15 +0100

    openiscsi: Unbreak.

--- pkgs/os-specific/linux/open-iscsi/default.nix
+++ pkgs/os-specific/linux/open-iscsi/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, kernel}:
 let
-  pname = "open-iscsi-2.0-871";
+  pname = "open-iscsi-2.0-873";
 in stdenv.mkDerivation {
   name = "${pname}-${kernel.version}";
   
   src = fetchurl {
     url = "http://www.open-iscsi.org/bits/${pname}.tar.gz";
-    sha256 = "1jvx1agybaj4czhz41bz37as076spicsmlh5pjksvwl2mr38gsmw";
+    sha256 = "1nbwmj48xzy45h52917jbvyqpsfg9zm49nm8941mc5x4gpwz5nbx";
   };
   
   KSRC = "${kernel.dev}/lib/modules/*/build";
@@ -16,12 +16,9 @@ in stdenv.mkDerivation {
     sed -i 's|/usr/|/|' Makefile
   '';
   
-  patches = [./kernel.patch];
-  
   meta = {
     description = "A high performance, transport independent, multi-platform implementation of RFC3720";
     license = stdenv.lib.licenses.gpl2Plus;
     homepage = http://www.open-iscsi.org;
-    broken = true;
   };
 }
